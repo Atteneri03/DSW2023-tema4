8527622a659f0eaebb175b732235c11bfc177fec
Atteneri Estévez <atteeste2003@gmail.com>
2023-11-15 19:13:40 +0000

    transaccion con consultas preparadas anidadas

--- transaccion2.php
+++ transaccion2.php
@@ -20,8 +20,15 @@ $stmt_delete->bind_param("i",$id);
 $link->autocommit(FALSE);
 $stmt_query->execute();
 
+$results = $stmt_query->get_result();
+$stmt_query->close();
 
-while($stmt_query->fetch()){
+
+while($client = $results->fetch_object()){
+  $id = $client->id;
+  $first_name = $client->first_name;
+  $cars = $client->cars;
+
   //Comprobar si cars == 0 para anular la transacción.
   if($cars==0){
     echo "Se aborta poque el cliente con id=$id tiene 0 coches";
